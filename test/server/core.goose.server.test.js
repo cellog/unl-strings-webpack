2695116a09b32912f9fdcc19921b49b8958c4169
Gregory Beaver <greg@chiaraquartet.net>
2016-08-02 10:38:59 -0500

    get assertions working.  First actual test

--- test/server/core.goose.server.test.js
+++ test/server/core.goose.server.test.js
@@ -1,6 +1,8 @@
-import { expect, sinon } from './assertions.js'
+import assertions from './assertions.js'
 import goose from '../../src/core/goose'
 import mongoose from 'mongoose'
+const sinon = assertions.sinon
+const expect = assertions.expect
 
 describe("core.goose", () => {
   it("should create a mongoose schema for User", () => {
@@ -8,6 +10,10 @@ describe("core.goose", () => {
   })
 
   it("should set the url to 'mongodb://localhost/unl'", () => {
-    const spy = sinon.spy()
+    const spy = { connect: sinon.spy() }
+    const t = new goose('foo://bar', spy)
+
+    expect(t).instanceof(goose)
+    expect(spy.connect).calledWith('foo://bar')
   })
 })
