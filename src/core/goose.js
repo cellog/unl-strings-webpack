3284a079c64ee2906588a908b6fb4f63ddb6314a
Gregory Beaver <greg@chiaraquartet.net>
2016-08-17 10:05:39 -0500

    set up the user document schema

--- src/core/goose.js
+++ src/core/goose.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import validator from 'validator'
 
 class Goose {
   constructor(url, mong = mongoose) {
@@ -11,9 +12,43 @@ class Goose {
   static User = mongoose.Schema({
     userid: {
       type: String,
-      required: true,
+      required: true
     },
-    lastLogin: Date,
+    lastLogin: {
+      type: Date,
+    },
+    lastUpdate: {
+      type: Date,
+      default: Date.now
+    },
+    instrument: {
+      type: String,
+      validate: {
+        validator: (v) => ['violin', 'viola', 'cello', 'bass', 'harp', 'guitar'].indexOf(v) !== -1,
+        message: '{VALUE} must be a stringed instrument'
+      },
+      required: true
+    },
+    userType: {
+      type: String,
+      validate: {
+        validator: (v) => ['faculty', 'student', 'gta'].indexOf(v) !== -1,
+        message: '{VALUE} must be faculty, student or gta'
+      },
+      required: true
+    },
+    firstName: String,
+    lastName: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      validate: {
+        validator: validator.isEmail,
+        message: '{VALUE} is not a valid email address'
+      }
+    }
   })
 }
 
